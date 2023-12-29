b6f8f92433adf101f0663a20a303319a94d1545f
MUZAMMILPERVAIZ <muzammilpervaiz925139@gmail.com>
2023-12-29 20:50:20 +0500

    Pass Custom Documents

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='longtrainer',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     description='Produciton Ready LangChain',
     long_description=open('README.md').read(),
